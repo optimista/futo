2c73c09d0647b31eb8111cb6dfac6fd180203d87
Viktor Futó <optimistavf@gmail.com>
2022-08-10 14:35:11 -0500

    Feed cursor repaired + reactStrictMode useEffect 2x run treated better

--- fb/src/app/core/Feed.js
+++ fb/src/app/core/Feed.js
@@ -13,13 +13,13 @@ const BATCH_LIMIT = 20;
 const Feed = ({ collection, Item = () => null, profileId, ready = true, sortBy = "timestamp" }) => {
   const [batches, setBatches] = useState([]),
         [fetching, setFetching, setHasMore] = useInfiniteScroll({ fetching: true, hasMore: true }),
+        initial = useRef(false), // reactStrictMode
         listeners = useRef([]),
         mounted = useMounted();
  
-  useEffect(() => () => listeners.current.map(l => l()), []); // reactStrictMode safe
+  useEffect(() => () => listeners.current.map(l => l()), []);
 
   useEffect(() => {
-    let unsubscribed = false;
     if (ready) {
       if (!fetching) return;
       const l = batches.length, batch = last(batches) || [];
@@ -27,28 +27,30 @@ const Feed = ({ collection, Item = () => null, profileId, ready = true, sortBy =
       let q = collection;
       if (profileId !== undefined) q = query(q, where("profileId", "==", profileId));  
       q = query(q, orderBy(sortBy, "desc"));
-      if (0 < l) q = query(q, startAfter(last(batch)[sortBy]));
-          
-      !unsubscribed && getDocs(query(q, limit(BATCH_LIMIT))).then(snapshot => {
-        if (mounted.current && !unsubscribed) { // prevent setting the state when component has been unmounted + prevent second initial fetch (reactStrictMode)
-          const items = snapshot.docs.map(doc => doc.data());
-          setBatches(bs => bs.concat([items]));
-          const unsubscribe = onSnapshot(query(q, endAt(last(items)?.[sortBy] || new Date())), snapshot => {
-            const itemsUpdated = snapshot.docs.map(doc => doc.data());
-            setBatches(bs => bs.map((b, i) => i === l ? itemsUpdated : b)); 
-          });
-          listeners.current = listeners.current.concat([unsubscribe]);
-          if (items.length < BATCH_LIMIT) setHasMore(false); 
+      if (0 < l) { q = query(q, startAfter(last(batch).doc)); } else { if (initial.current) return; } // reactStrictMode
+         
+      getDocs(query(q, limit(BATCH_LIMIT))).then(snapshot => {
+        if (mounted.current) { // prevent setting the state when component has been unmounted
+          if (0 < snapshot.docs.length) {
+            const map = doc => ({ doc: doc, data: doc.data() }), items = snapshot.docs.map(map);
+            setBatches(bs => bs.concat([items]));
+            const unsubscribe = onSnapshot(query(q, endAt(last(items).doc)), snapshot => {
+              const itemsUpdated = snapshot.docs.map(map);
+              setBatches(bs => bs.map((b, i) => i === l ? itemsUpdated : b)); 
+            });
+            listeners.current = listeners.current.concat([unsubscribe]);
+          }
+          if (snapshot.docs.length < BATCH_LIMIT) setHasMore(false); 
           setFetching(false);
         }
       });
     }
-    return () => { unsubscribed = true; }
+    return () => { initial.current = true; } // reactStrictMode
   }, [fetching, ready]);
 
   return (
     <>
-      { batches.map(batch => batch.map(doc => Item(doc, doc.id))) }
+      { batches.map(batch => batch.map(({ data }) => Item(data, data.id))) }
       
       {/* Additional two Item Skeletons for initial load (together = 3) */}
       { fetching && batches.length === 0 && (new Array(3-1)).fill().map((_, i) => Item(_, "s" + i)) }
